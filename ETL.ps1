e416cdcbd8ff9d3726a3d7a84cfa05911d1851aa
Garin J Tanner <32025331+GarinJTanner@users.noreply.github.com>
2023-02-21 11:36:16 -0800

    Update ETL.ps1

--- ETL.ps1
+++ ETL.ps1
@@ -6,7 +6,7 @@ set-variable -name "iterations" -value ($file | Measure-Object -Line)
 
 $variable = 0
 
-write-output "insert into locations (latitude,longitude,location) VALUES ">C:\Users\G\OneDrive\Tech\ETL\output\shakeshack.sql
+write-output "insert into locations (latitude,longitude,location) VALUES ">C:\Users\...\...\output\shakeshack.sql
 
 While ($variable -LT $iterations.lines)
 
@@ -16,7 +16,7 @@ set-variable -name "latitude" -value ( $target -split ',' |select -index 0 )
 set-variable -name "longitude" -value ( $target -split ',' |select -index 1 )
 set-variable -name "location" -value ( $target -split ',' |select -index 2 )
 $location = $location -replace "'",""
-write-output "('$latitude','$longitude','$location')," >>C:\Users\G\OneDrive\Tech\ETL\output\shakeshack.sql
+write-output "('$latitude','$longitude','$location')," >>C:\Users\...\...output\shakeshack.sql
 
 $variable++
 }
